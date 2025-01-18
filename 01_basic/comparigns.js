4b450d31e7b136ccc182bb96b3e763b2e692ee8b
Calladitya <aditya0812tiwari@gmail.com>
2025-01-18 09:27:25 +0000

    comparigns

---
+++ 01_basic/comparigns.js
@@ -0,0 +1,15 @@
+//here we do comparign between two data types 
+//console.log("2" > 1)//ye  to true print kr raha h lekin kese jbki ek value to hamari string me h or ek value hamri no ye ek drarback h java script ka
+//console.log("02" > 1)// ''
+
+console.log(null >  0)//ab ye print krta h ki false
+console.log(null == 0)//yaha pe bhi print krta h false//yaha pe comparison operator null ko 0 ke equal nhi krta 
+console.log(null >= 0)//yaha pe print krta h true ki null badi value h zero se//yaha kya hota ki jb bhi hm comparison krte h to jo comparison operator hoya h vo null ko 0 me convert kr deta h
+//ye jo comparison operator change krta h null ko 0 me vo kaam sirf >= or <= me hi hota h 
+
+
+//===//ye hamara jo triple equal h vo num bers to check krta hi h leki usi ke saath data type bhi check krta h
+//example
+console.log("2"==2)//agar iska output dekhege to ye hame true dega kyu ki double  equal dono ko number manta h 
+console.log("2"===2)//yaha  agar hm output dekhte h to pta chelega ki ye number ke saath datatyprs bhi check krta h 
+//or agar hm uper dekhe to hmme pta chalega ki ek value str h or ek value number jo ki kabhi bhi equal ho hi nhi sakti
